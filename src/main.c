9727e5e226f6a9bc13426ddaf3f3cf3e577cc6fd
moi24py <moi24py@gmail.com>
2025-09-27 18:17:59 +0200

    add initialize sniffing to main.c

--- src/main.c
+++ src/main.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 #include <pcap.h>
 
-// Get program name and network interface name
+// 1. Get program name and network interface name
 int main(int argc, char *argv[])
 {
-    // if user does not provide two arguments
+    // If user does not provide two arguments
     if (argc < 2) {
-        // print an usage error message and exit the program
+        // Print an usage error message and exit the program
         fprintf(stderr, "Usage: %s <network interface>\n"
                 "You must pass 2 arguments:\n"
                 "1) The program name\n"
-                "2) The network interface name\n", argv[0] //program name
+                "2) The network interface name\n", argv[0] //Program name
             );
         return 1;
     }
 
-    // Network interface to sniff on
+    // 2. Initialize: open the NI for sniffing
 	char *net_int = argv[1];
+    printf("Selected Network Interface: %s\n", net_int);
+    
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *handle;
+    handle = pcap_open_live(net_int, BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+	    fprintf(stderr, "Couldn't open Network Interface: \"%s\"\n"
+                        "%s\n",
+                        net_int, errbuf);
+	    return 2;
+    }
 
-	printf("Network interface: %s\n", net_int);
-	return(0);
+	return 0;
 }
